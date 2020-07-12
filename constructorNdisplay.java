21bbaa50eafea0ccd2ada512c842d7768282281d
Abhay <chauhan27.abhay@gmail.com>
2020-07-12 12:27:17 +0530

    01 constructor and Display

---
+++ constructorNdisplay.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+class constructorNdisplay{
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static class Pair{
+        Node node;
+        int state;
+
+        Pair(Node node, int state){
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static void display(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.left == null ? "." : node.left.data + "");
+        System.out.print(" <- " + node.data + " -> ");
+        System.out.println(node.right == null ? "." : node.right.data + "");
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void main(String [] args){
+        int [] arr = {50,25,12,-1,-1,37,30,-1,-1,-1,75,62,-1,70,-1,-1,87,-1,-1};
+
+        Node root  = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while(st.size()>0){
+            Pair top = st.peek();
+            if(top.state == 1){
+                idx++;
+                if(arr[idx] == -1){
+                    top.node.left = null;
+                }else{
+                    Node ln = new Node(arr[idx], null, null);
+                    top.node.left = ln;
+                    Pair lp = new Pair(ln, 1);
+                    st.push(lp);
+                }
+                top.state++;
+            }else if(top.state == 2){
+                idx++;
+                if(arr[idx] == -1){
+                    top.node.right = null;
+                }else{
+                    Node rn = new Node(arr[idx], null, null);
+                    top.node.right = rn;
+                    Pair rp = new Pair(rn, 1);
+                    st.push(rp);
+                }
+                top.state++;
+            }else{
+                st.pop();
+            }
+        } display(root);
+
+    }
+}
