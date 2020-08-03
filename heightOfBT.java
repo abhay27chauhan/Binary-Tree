731d1496ebad90878d9f4fcf45dedff66977ed46
Abhay <chauhan27.abhay@gmail.com>
2020-08-03 17:41:23 +0530

    05 height of Binary Tree

---
+++ heightOfBT.java
@@ -0,0 +1,80 @@
+import java.util.Stack;
+class heightOfBT{
+    private static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static class Pair{
+        Node node;
+        int state;
+
+        Pair(Node node, int state){
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node constructor(int[] arr){
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+        int idx =0;
+        while(st.size()>0){
+            Pair top = st.peek();
+            if(top.state == 1){
+                idx++;
+                if(arr[idx] != -1){
+                    Node ln = new Node(arr[idx], null, null);
+                    top.node.left = ln;
+                    Pair lp = new Pair(ln, 1);
+                    st.push(lp);
+                }else{
+                    top.node.left = null;
+                }
+                top.state++;
+            }else if(top.state == 2){
+                idx++;
+                if(arr[idx] != -1){
+                    Node rn = new Node(arr[idx], null, null);
+                    top.node.right = rn;
+                    Pair rp = new Pair(rn, 1);
+                    st.push(rp);
+                }else{
+                    top.node.right = null;
+                }
+                top.state++;
+            }else {
+                st.pop();
+            }
+        } return root;
+    }
+
+    public static int heightOfBT(Node node){
+        if(node == null){
+            return -1;
+        }
+
+        int lh = heightOfBT(node.left);
+        int rh = heightOfBT(node.right);
+
+        int oheight = Math.max(lh,rh) + 1;
+        return oheight;
+    }
+
+    public static void main(String[] args){
+        int [] arr = {50,25,12,-1,-1,37,30,-1,-1,-1,75,62,-1,70,-1,-1,87,-1,-1};
+
+        Node root = constructor(arr);
+        int height = heightOfBT(root);
+        System.out.println(height);
+    }
+}
